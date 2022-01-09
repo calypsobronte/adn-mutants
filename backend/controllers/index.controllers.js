3db26c853a79e4bf11f0d25b1fd0c3572ecebc3d
Lina María Montaño Ramírez - @calypsobronte <codelinamaria@gmail.com>
2022-01-09 13:19:24 -0500

    subida de index

--- backend/controllers/index.controllers.js
+++ backend/controllers/index.controllers.js
@@ -1,3 +1,19 @@
 export const indexDatos = async (req, res) => {
-    res.json({"Recopilacion de datos": "Bienvenidos a la plataforma."})
+    res.json({"Recopilacion de datos": [
+        {
+            "Autor": {
+                "nombre": "Lina Maria Montaño Ramirez",
+                "url": ["https://github.com/calypsobronte", "https://www.linkedin.com/in/calypsobronte/"]
+            },
+            "Lista de secuencias ADN": "https://adn-mutants.herokuapp.com/api/magneto/all",
+            "Detectar mutante": [
+                {
+                    "url": "https://adn-mutants.herokuapp.com/api/magneto/mutant",
+                    "Ingresa el ejemplo": {"dna": ["ATG","CAG","TTA"]}
+                }
+            ],
+            "Lista de estadistica": "https://adn-mutants.herokuapp.com/api/magneto/stats",
+            "__v": 0
+        }
+    ]})
 }
